b337eb22432de659a411b258bdd02fa14608d32b
ashuisalluneed <zadavashish1995@gmail.com>
2026-03-15 01:26:50 +0530

    feat: add dynamic sitemap generation for videos and users.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://bipbop.com";
 
